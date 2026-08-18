747cadd6700c7bd52eb7b6d62c1c0131ec00033f
sarthakgupta123rev-design <sarthakgupta123rev@gmail.com>
2026-08-18 23:50:00 +0530

    updated

---
+++ func in js/func.js
@@ -0,0 +1,7 @@
+let a = prompt("enter value of a :" );
+let b = prompt("enter value of b :" );
+function multiply(a,b){
+    return a*b;
+}
+let result = multiply(a,b);
+document.write("result is: " + result+" multiplication of "+ a +" and "+b);
