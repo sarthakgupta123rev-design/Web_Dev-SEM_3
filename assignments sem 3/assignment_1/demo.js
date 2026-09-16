9ab5a7e48f641ad9d78e794a10eb44a846919f1c
sarthakgupta123rev-design <sarthakgupta123rev@gmail.com>
2026-09-16 23:34:17 +0530

    Added assignment 1

---
+++ assignments sem 3/assignment_1/demo.js
@@ -0,0 +1,10 @@
+function quiz(){
+            let name = document.getElementById("name").value;
+            let roll = document.getElementById("roll").value;
+            let section = document.getElementById("section").value;
+}
+
+// const
+// {
+    
+// }
